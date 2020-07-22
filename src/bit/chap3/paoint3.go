31cc91807f4623f3f73d0f3b0fcfee2e913e8049
jacoloves <5511068t@gmail.com>
2020-07-22 18:11:13 +0900

    2020-07-22

---
+++ src/bit/chap3/paoint3.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+	S    string
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000
+}
+
+func main() {
+	v := Vertex{1, 2, "test2"}
+	changeVertex(v)
+	fmt.Println(v)
+
+	v2 := &Vertex{1, 2, "test2"}
+	changeVertex2(v2)
+	fmt.Println(v2)
+}
--- src/current/stringer.go
+++ src/current/stringer.go
@@ -1,18 +1,18 @@
-package main
+	package main
 
-import "fmt"
+	import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
-}
+	type Person struct {
+		Name string
+		Age  int
+	}
 
-func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
-}
+	func (p Person) String() string {
+		return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+	}
 
-func main() {
-	a := Person{"Donald", 42}
-	z := Person{"Barak", 43}
-	fmt.Println(a, z)
-}
+	func main() {
+		a := Person{"Donald", 42}
+		z := Person{"Barak", 43}
+		fmt.Println(a, z)
+	}
