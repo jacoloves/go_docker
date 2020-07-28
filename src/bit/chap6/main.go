e85a4103c45be84242ec5ced6d3cf8166b11c94d
jacoloves <5511068t@gmail.com>
2020-07-29 01:39:06 +0900

    2020-07-29

--- src/bit/chap6/main.go
+++ src/bit/chap6/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"package/mylib"
+	"package/mylib/under"
 )
 
 func main() {
 	s := []int{1, 2, 3, 4, 5}
 	fmt.Println(mylib.Average(s))
+
+	mylib.Say()
+	under.Hello()
+	person := mylib.Person{Name: "Mike", Age: 20}
+	fmt.Println(person)
+
+	fmt.Println(mylib.Public)
+	fmt.Println(mylib.private)
 }
