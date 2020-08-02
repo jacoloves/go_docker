2061f5c91b47e8167c7be834fd49b27154b5c988
jacoloves <5511068t@gmail.com>
2020-08-02 14:23:06 +0900

    2020-08-02

---
+++ src/bit/chap7/sort.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	i := []int{5, 3, 2, 8, 7}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Nancy", 20},
+		{"Vera", 40},
+		{"Mike", 30},
+		{"Bob", 50},
+	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	sort.Strings(s)
+	fmt.Println(i, s, p)
+}
