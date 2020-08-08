2f6db5032eeb6a74ddb40227b6876aef3852206f
jacoloves <5511068t@gmail.com>
2020-08-08 22:25:29 +0900

    2020-08-08

---
+++ src/bit/chap7/unmarshal.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type T struct{}
+
+type Person struct {
+	Name     string   `json:"name,omitempty"`
+	Age      int      `json:"age,omitempty"`
+	Nickname []string `json:"nicknames,omitempty"`
+	T        *T       `json:"T,omitempty"`
+}
+
+func (p *Person) UnmarshalJSON(b []byte) error {
+	type Person2 struct {
+		Name string
+	}
+	var p2 Person2
+	err := json.Unmarshal(b, &p2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.Name = p2.Name + "!"
+	return err
+}
+
+func main() {
+	b := []byte(`{"name":"mike","age":20,"nicknames":{}}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nickname)
+
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
+}
