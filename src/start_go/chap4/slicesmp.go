a29ebf3534aa914fb6f77938b5e41cae3bcc6fe8
jacoloves <5511068t@gmail.com>
2020-10-06 23:55:28 +0900

    20201006

--- src/start_go/chap4/slicesmp.go
+++ src/start_go/chap4/slicesmp.go
@@ -37,6 +37,49 @@ func easySla() {
 
 	s1 := "ABCDE"[1:3]
 	fmt.Println(s1)
+
+	s2 := "あいうえお"[3:9]
+	fmt.Println(s2)
+
+	s3 := []int{1, 2, 3}
+	s3 = append(s3, 4)
+	fmt.Println(s3)
+
+	s3 = append(s3, 5, 6, 7)
+	fmt.Println(s3)
+
+	s4 := []int{1, 2, 3}
+	s5 := []int{4, 5, 6}
+
+	s6 := append(s4, s5...)
+	fmt.Println(s6)
+
+	var b []byte
+	b = append(b, "あいうえお"...)
+	b = append(b, "かきくけこ"...)
+	b = append(b, "さしすせそ"...)
+	fmt.Println(b)
+
+	s7 := make([]int, 0, 0)
+	fmt.Printf("(A) len=%d, cap=%d\n", len(s7), cap(s7))
+
+	s7 = append(s7, 1)
+	fmt.Printf("(B) len=%d, cap=%d\n", len(s7), cap(s7))
+
+	s7 = append(s7, []int{2, 3, 4}...)
+	fmt.Printf("(C) len=%d, cap=%d\n", len(s7), cap(s7))
+
+	s7 = append(s7, 5)
+	fmt.Printf("(D) len=%d, cap=%d\n", len(s7), cap(s7))
+
+	s7 = append(s7, 6, 7, 8, 9)
+	fmt.Printf("(E) len=%d, cap=%d\n", len(s7), cap(s7))
+
+	s8 := make([]int, 1024, 1024)
+	fmt.Printf("len=%d, cap~%d\n", len(s8), cap(s8))
+
+	s8 = append(s8, 0)
+	fmt.Printf("len=%d, cap~%d\n", len(s8), cap(s8))
 }
 
 func sliceSmp() {
